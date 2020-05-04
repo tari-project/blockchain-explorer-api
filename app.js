c60fc5714b89312d728ccce293df5ebe7830d395
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-05-04 14:09:42 +0200

    Installing packages

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cors = require("cors");
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
+
+const indexRouter = require('./routes/index');
+
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
