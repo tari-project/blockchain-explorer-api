5cfe4495bc9f111214b23399cd12d5ce16101632
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-05-04 14:14:15 +0200

    Structure app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-const app = express()
 
-const indexRouter = require('./routes/index')
+const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
@@ -13,6 +12,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+/** Routes */
+const indexRouter = require('./routes/index')
 
 app.use('/', indexRouter)
 
