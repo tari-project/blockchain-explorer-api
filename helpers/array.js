f8cf9df2a2745c79aa805af80c02ebf503ec99c1
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-06-02 15:33:07 +0200

    Don't allow negative array size

--- helpers/array.js
+++ helpers/array.js
@@ -1,7 +1,7 @@
 function range (startAt = 0, size, zeroBasedArray) {
   startAt = zeroBasedArray ? startAt : startAt + 1
   size = zeroBasedArray ? size : size + 1
-  return [...Array(+size).keys()].map(i => i + +startAt)
+  return [...Array(Math.max(+size, 0)).keys()].map(i => i + +startAt)
 }
 
 module.exports = {
