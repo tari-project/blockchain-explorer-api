d76fe5c4d799e9cc4fc9f2c6da8b6545864a9d63
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-06-03 09:58:29 +0200

    Use GRPC for tokens in circulation

--- helpers/array.js
+++ helpers/array.js
@@ -4,6 +4,12 @@ function range (startAt = 0, size, zeroBasedArray) {
   return [...Array(Math.max(+size, 0)).keys()].map(i => i + +startAt)
 }
 
+function rangeInclusive (start, end) {
+  const size = Math.max(end - start + 1, 0)
+  return [...Array(size).keys()].map(i => i + +start)
+}
+
 module.exports = {
-  range
+  range,
+  rangeInclusive
 }
