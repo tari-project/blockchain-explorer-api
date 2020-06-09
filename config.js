49b16cbc1c5dc20b4540563b49ba5bd089d0df40
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2020-06-09 11:30:43 +0200

    Added metadata to websocket call (#24)
    * Added websocket broadcast to metadata
    
    * Moved functions to model
    
    * Remove unused var

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
   redisUrl: process.env.REDIS_URL,
   adminToken: process.env.ADMIN_TOKEN,
   grpcUrl: process.env.GRPC_URL,
-  protoRemoteUrl: process.env.PROTO_REMOTE_URL
+  protoRemoteUrl: process.env.PROTO_REMOTE_URL,
+  blockTimeSeconds: process.env.BLOCK_TIME_SECONDS || 120
 }
 module.exports = config
--- helpers/redis.js
+++ helpers/redis.js
@@ -18,4 +18,14 @@ const client = {
   zrange: promisify(syncClient.zrange).bind(syncClient),
   flushall: promisify(syncClient.flushall).bind(syncClient)
 }
-module.exports = client
+const REDIS_STORE_KEYS = {
+  BLOCK_CURRENT_HEIGHT: 'current_block_height',
+  DIFFICULTY_CURRENT_HEIGHT: 'current_difficulty_height',
+  TRANSACTIONS_BY_TIMESTAMP: 'transactions_by_time',
+  TRANSACTIONS_TOTAL: 'transactions_total',
+  DIFFICULTIES_BY_HEIGHT: 'difficulties_by_height',
+  BLOCKS_BY_HEIGHT: 'blocks_by_height',
+  BLOCKS_BY_TIME: 'blocks_by_time',
+  CONSTANTS: 'constants'
+}
+module.exports = { client, REDIS_STORE_KEYS }
